45b19c99545680d1c0a9bf6213b209fb5a8704eb
CL-ModelCloud <cl@modelcloud.ai>
2025-02-10 02:38:10 +0000

    code review

--- tokenicer/const.py
+++ tokenicer/const.py
@@ -12,6 +12,6 @@ PAD_TOKEN_TUPLE = namedtuple("PadTokenTuple", ["pad_token", "pad_token_id"])
 
 MODEL_PAD_TOKEN_MAP = {
         "llama": PAD_TOKEN_TUPLE(pad_token='<|finetune_right_pad_id|>', pad_token_id=128004),
-        "qwen2_5_vl": PAD_TOKEN_TUPLE(pad_token='<|vision_pad|>', pad_token_id='151654'),
+        "qwen2_5_vl": PAD_TOKEN_TUPLE(pad_token='<|vision_pad|>', pad_token_id=151654),
         "qwen2": PAD_TOKEN_TUPLE(pad_token='<|fim_pad|>', pad_token_id=151662),
 }
