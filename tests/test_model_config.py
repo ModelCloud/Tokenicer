7c10bb9d9dc2e0a2017a7aace1c0ac2cfc140d63
CL-ModelCloud <cl@modelcloud.ai>
2025-02-11 12:06:22 +0800

    test code clean up (#26)
    * test code clean up
    
    * code clean up
    
    * code revert

--- tests/test_model_config.py
+++ tests/test_model_config.py
@@ -30,11 +30,11 @@ class TestModelConfig(unittest.TestCase):
         self.assertEqual(
             tokenicer.model_config.bos_token_id,
             expect_bos_token_id,
-            msg=f"Expected bos_token_id='{expect_bos_token_id}' but got '{tokenicer.model_config.bos_token_id}'."
+            msg=f"Expected bos_token_id: `{expect_bos_token_id}`, actual=`{tokenicer.model_config.bos_token_id}`."
         )
 
         self.assertEqual(
             tokenicer.model_config.eos_token_id,
             expect_eos_token_id,
-            msg=f"Expected eos_token_id='{expect_eos_token_id}' but got '{tokenicer.model_config.eos_token_id}'."
+            msg=f"Expected eos_token_id: `{expect_eos_token_id}`, actual=`{tokenicer.model_config.eos_token_id}`."
         )
